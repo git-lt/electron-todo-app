b0ac63a8ba761bbdf61da055851b3bdbd12eca6f
liutao <liutaotao@mytijian.com>
2023-07-20 15:14:00 +0800

    feat: ✨添加一张日志表

---
+++ server/prisma/migrations/20230720071337_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE `log` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `action` VARCHAR(191) NOT NULL,
+    `content` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `timestamp` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `log` ADD CONSTRAINT `log_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `sys_user`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
